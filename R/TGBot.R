79e554d2b54eed25e29c92f40e8c8dd1e10a841b
Luca Braglia <lbraglia@gmail.com>
2015-12-24 08:28:06 +0100

    misc lines

--- R/TGBot.R
+++ R/TGBot.R
@@ -1,3 +1,10 @@
+## ------
+## UTILS
+## ------
+
+self <- 'shut up R CMD CHECK'
+private <- 'shut up R CMD CHECK'
+
 initialize <- function(token) {
     self$set_token(token)
 }
@@ -20,11 +27,16 @@ request <- function(method, body){
     httr::POST(url = api_url, body = body)
 }
 
+
+## ------
+## TG API
+## ------
+
 getMe <- function() {
     r <- private$request('getMe')
-    status <- status_code(r)
+    status <- httr::status_code(r)
     if (status == 200){
-        c <- content(r)
+        c <- httr::content(r)
         bot_first_name <- c$result$first_name
         bot_username <- c$result$username
         cat(sprintf('Bot name:\t%s\nBot username:\t%s\n',
@@ -36,27 +48,51 @@ getMe <- function() {
     }
 }
 
-getUpdates <- function(){
-    r <- private$request('getUpdates')
-    if (r$status == 200)
-        return(r)
-    else
-        return(NULL)
-}
-
-## ----
-## SEND
-## ----
 
 sendMessage <- function(text, chat_id){
-    if (missing(chat_id))
-        chat_id <- private$default_chat_id
+    if (missing(chat_id)) chat_id <- private$default_chat_id
     r <- private$request('sendMessage', 
                          body = list(
                              'chat_id' = chat_id,
                              'text' = as.character(text)))
     invisible(r)
 }
+
+
+forwardMessage <- function(from_chat_id, message_id, chat_id){
+    if (missing(chat_id)) chat_id <- private$default_chat_id
+    if (missing(from_chat_id) ||  missing(message_id))
+        stop("forwardMessage: from_chat_id and message_id can't be missing")
+    
+    r <- private$request('forwardMessage', 
+                         body = list(
+                             'chat_id' = chat_id,
+                             'from_chat_id' = chat_id,
+                             'message_id' = as.character(message_id)))
+    invisible(r)
+}
+
+sendPhoto <- function(photo, caption, reply_to_message_id,
+                      reply_markup, chat_id){
+    if (missing(chat_id)) chat_id <- private$default_chat_id
+    if (missing(photo)) stop("sendPhoto: photo can't be missing")
+    if (missing(caption)) caption <- NULL
+    if (missing(reply_to_message_id)) reply_to_message_id <- NULL
+    if (missing(reply_markup)) reply_markup <- NULL
+
+   
+    r <- private$request('forwardMessage', 
+                         body = list(
+                             'chat_id' = chat_id,
+                             'photo' = httr::upload_file(photo)## ,
+                             ## 'caption' = caption,
+                             ## 'reply_to_message_id' = reply_to_message_id,
+                             ## 'reply_markup' = reply_markup
+                         ))
+    invisible(r)
+
+}
+
 
 sendDocument <- function(f, chat_id){
     if (missing(chat_id))
@@ -66,14 +102,29 @@ sendDocument <- function(f, chat_id){
     r <- private$request('sendDocument',
                          body = list(
                              'chat_id' = chat_id,
-                             'document' = upload_file(f)))
+                             'document' = httr::upload_file(f)))
     invisible(r)
 }
 
 
+getUpdates <- function(){
+    r <- private$request('getUpdates')
+    if (r$status == 200){
+        ## parse output (return a data.frame)
+        rval <- httr::content(r)$result
+        do.call(rbind, lapply(rval, as.data.frame))
+    }
+    else
+        invisible(NULL)
+}
+
+
+## ----------
+## Main Class
+## ----------
 
 #' TGBot
-#' 
+#'
 #' @export
 TGBot <- R6::R6Class("TGBot",
                      public = list(
@@ -85,6 +136,8 @@ TGBot <- R6::R6Class("TGBot",
                          getMe = getMe,
                          getUpdates = getUpdates,
                          sendMessage = sendMessage,
+                         forwardMessage = forwardMessage,
+                         sendPhoto = sendPhoto,
                          sendDocument = sendDocument                     
                      ),
                      private = list(
@@ -92,17 +145,3 @@ TGBot <- R6::R6Class("TGBot",
                          default_chat_id = NA,
                          request = request)
                      )
-
-
-
-## bot <- TGBot$new(token = '163188543:AAHH8lwXaTCxnMsixmg5Y6aukCWbv-_jgME')
-## bot$getMe()
-## ## now find and say something to your bot on the phone
-## b <- bot$getUpdates()
-## bot$set_default_chat_id(162174388)
-
-## bot$sendMessage('This is a test')
-## png('test.png')
-## plot(rnorm(100))
-## dev.off()
-## d <- bot$sendDocument('test.png')
--- R/telegram-package.r
+++ R/telegram-package.r
@@ -4,6 +4,7 @@
 #' @docType package
 #' @importFrom R6 R6Class
 #' @importFrom httr POST
+#' @importFrom httr content
 #' @importFrom httr status_code
 #' @importFrom httr upload_file
 NULL
