7bfbeb1730fc2a530b9164a8446b3237b0634c1b
Juan M. Carrillo <juanmacarrillo@gmail.com>
2019-06-16 17:43:59 +0200

    - Added 'caption' option to sendAudio. - Added 'disable_web_page_preview' to 'make_body' in 'sendMessage'

--- R/TGBot.R
+++ R/TGBot.R
@@ -273,11 +273,13 @@ getUserProfilePhotos <- function(user_id = NULL,
 #' @param duration duration of the audio in seconds
 #' @param performer performer
 #' @param title track name
+#' @param caption audio caption
 #' @param reply_to_message_id If the message is a reply, ID of the
 #'     original message
 #' @param chat_id Unique identifier for the target chat or username of
 #'     the target channel (required)
 sendAudio <- function(audio = NULL,
+                      caption = NULL,
                       duration = NULL,
                       performer = NULL,
                       title = NULL,
@@ -286,6 +288,7 @@ sendAudio <- function(audio = NULL,
 {
     ## params
     chat_id <- private$check_chat_id(chat_id = chat_id)
+    caption <- check_param(caption, 'char')
     audio <- check_file(audio, required = TRUE)
     duration <- check_param(duration, 'int')
     performer <- check_param(performer, 'char')
@@ -294,6 +297,7 @@ sendAudio <- function(audio = NULL,
     ## request body
     body <- make_body('chat_id' = chat_id,
                       'audio' = httr::upload_file(audio),
+                      'caption' = caption,
                       'duration' = duration,
                       'performer' = performer,
                       'title' = title,
@@ -390,6 +394,7 @@ sendMessage <- function(text = NULL,
     body <- make_body('chat_id' = chat_id,
                       'text' = as.character(text),
                       'parse_mode' = parse_mode,
+                      'disable_web_page_preview' = disable_web_page_preview,
                       'reply_to_message_id' = reply_to_message_id)
     ## request
     r <- private$request('sendMessage', body = body)
